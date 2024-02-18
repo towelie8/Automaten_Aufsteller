bdeabeb9ca851d540ec322162b15de15ca89d1c4
m4x <maximilian6695@web.de>
2024-02-18 08:38:49 +0100

    carousel-control-next - click function

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,8 @@ var carouselWidth = $('.carousel-inner')[0].scrollWidth;
 var cardWith = $('.carousel-item').width();
 
 $('.carousel-control-next').on('click', function(){
+    console.log('next');
     scrollPosition = scrollPosition + cardWidth;
-})
+    $('.carousel-inner').animate({scrollLeft: scrollPosition},
+        600)
+});
